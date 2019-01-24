846fa10ce07cf6f421a03e947987f80cbc3c7d49
Prashant Gupta <prashantgupta@us.ibm.com>
2019-01-23 17:31:26 -0800

    multi go-routine control

--- Explore/test.go
+++ Explore/test.go
@@ -2,39 +2,75 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
 type str struct {
-	val int
+	val1 int
+	val2 int
 }
 
-func createJob(val int) (string, error) {
+func createJob(val1, val2 int) (string, error) {
 	st := &str{}
-	errChan := make(chan error)
-	go func(errChan chan error) {
-		defer close(errChan)
-		if val > 1 {
-			st.val = val
-			time.Sleep(time.Second * 2)
+
+	var wg sync.WaitGroup
+	errChan1 := make(chan error, 2)
+	//errChan2 := make(chan error)
+
+	wg.Add(1)
+	go func(wg *sync.WaitGroup, errChan chan error) {
+		defer wg.Done()
+		if val1 > 1 {
+			st.val1 = val1
+			time.Sleep(time.Second * 3)
+			errChan <- nil
 		} else {
-			err := fmt.Errorf("invalid amount %v", val)
+			err := fmt.Errorf("too much for val1 : %v", val1)
 			errChan <- err
 		}
-	}(errChan)
+	}(&wg, errChan1)
 
-	for err := range errChan {
+	wg.Add(1)
+	go func(errChan chan error) {
+		defer wg.Done()
+
+		if val2 < 0 {
+			st.val2 = val2
+			//time.Sleep(time.Second * 2)
+			errChan <- nil
+		} else {
+			err := fmt.Errorf("too less for val2 : %v", val2)
+			errChan <- err
+		}
+	}(errChan1)
+
+	go func(wg *sync.WaitGroup, errChan chan error) {
+		wg.Wait()
+		close(errChan)
+	}(&wg, errChan1)
+
+	for err := range errChan1 {
 		if err != nil {
 			return "", err
 		}
 	}
-	return fmt.Sprintf("Struct is: %v", st), nil
+	return fmt.Sprintf("Values are correct! Struct is: %v", st), nil
 }
-func main() {
-	if s, err := createJob(-4); err != nil {
-		panic(err)
+
+func display(s string, err error) {
+	if err != nil {
+		fmt.Println("ERROR!! >>> ", err)
 	} else {
 		fmt.Println(s)
 	}
+}
+
+func main() {
+
+	display(createJob(4, 2))
+	display(createJob(-4, -2))
+	display(createJob(-4, 2))
+	display(createJob(4, -2))
 
 }
