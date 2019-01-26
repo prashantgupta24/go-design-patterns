822a23d3ea243d3f9c63a017585bb1f1d2f79f7e
Prashant Gupta <prashantgupta@us.ibm.com>
2019-01-25 17:42:24 -0800

    added concurrent package

---
+++ Concurrent/Barrier/test.go
@@ -0,0 +1,181 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+type str struct {
+	val1 int
+	val2 int
+}
+
+type returnVal struct {
+	msg string
+	err error
+}
+
+type barrier struct {
+	wg        *sync.WaitGroup
+	results   chan *returnVal
+	functions []func()
+}
+
+func (b *barrier) init() {
+	var wg sync.WaitGroup
+	b.wg = &wg
+	b.results = make(chan *returnVal)
+}
+
+func (b *barrier) add(fn func()) {
+	b.functions = append(b.functions, fn)
+}
+
+func (b *barrier) execute() (string, error) {
+
+	for _, fn := range b.functions {
+		b.wg.Add(1)
+		go fn()
+	}
+	// wg.Add(1)
+	// go job1(val1, &wg, results, st)
+
+	// wg.Add(1)
+	// go job2(val2, &wg, results, st)
+
+	go func(wg *sync.WaitGroup, results chan *returnVal) {
+		wg.Wait()
+		close(results)
+	}(b.wg, b.results)
+
+	for result := range b.results {
+		if result.err != nil {
+			return "", result.err
+		}
+	}
+	return fmt.Sprintf("Values are correct! Struct is:"), nil
+}
+
+// func (b *barrier) wait() (string, error) {
+// 	go func(wg *sync.WaitGroup, results chan *returnVal) {
+// 		wg.Wait()
+// 		close(results)
+// 	}(b.wg, b.results)
+
+// 	for result := range b.results {
+// 		if result.err != nil {
+// 			return "", result.err
+// 		}
+// 	}
+// 	return fmt.Sprintf("Correct!"), nil
+// }
+
+func (b *barrier) wait() []*returnVal {
+	go func(wg *sync.WaitGroup, results chan *returnVal) {
+		wg.Wait()
+		close(results)
+	}(b.wg, b.results)
+
+	var returnValues []*returnVal
+	for result := range b.results {
+		returnValues = append(returnValues, result)
+	}
+	return returnValues
+}
+func job1(barrier *barrier, val int, st *str) {
+	defer barrier.wg.Done()
+
+	time.Sleep(time.Second * 3)
+	if val > 1 {
+		st.val1 = val
+		barrier.results <- &returnVal{
+			msg: "success",
+			err: nil,
+		}
+	} else {
+		err := fmt.Errorf("too much for val in func1 : %v", val)
+		barrier.results <- &returnVal{
+			msg: "",
+			err: err,
+		}
+	}
+}
+
+func job2(barrier *barrier, val int, st *str) {
+	defer barrier.wg.Done()
+
+	time.Sleep(time.Second * 2)
+	if val < 0 {
+		st.val2 = val
+		barrier.results <- &returnVal{
+			msg: "success",
+			err: nil,
+		}
+	} else {
+		err := fmt.Errorf("too less for val in func2: %v", val)
+		barrier.results <- &returnVal{
+			msg: "",
+			err: err,
+		}
+	}
+}
+
+func createJob(val1, val2 int) (string, error) {
+	st := &str{}
+
+	barrier := &barrier{}
+	barrier.init()
+
+	barrier.wg.Add(1)
+	go job1(barrier, val1, st)
+
+	barrier.wg.Add(1)
+	go job2(barrier, val2, st)
+
+	returnValues := barrier.wait()
+
+	for _, returnValue := range returnValues {
+		if returnValue.err != nil {
+			return "", returnValue.err
+		}
+	}
+	return fmt.Sprintf("Values are correct! Struct is: %v", st), nil
+	// var wg sync.WaitGroup
+	// results := make(chan *returnVal, 2)
+
+	// wg.Add(1)
+	// go job1(val1, &wg, results, st)
+
+	// wg.Add(1)
+	// go job2(val2, &wg, results, st)
+
+	// go func(wg *sync.WaitGroup, results chan *returnVal) {
+	// 	wg.Wait()
+	// 	close(results)
+	// }(&wg, results)
+
+	// for result := range results {
+	// 	if result.err != nil {
+	// 		return "", result.err
+	// 	}
+	// }
+	// return fmt.Sprintf("Values are correct! Struct is: %v", st), nil
+}
+
+func display(s string, err error) {
+	if err != nil {
+		fmt.Println("ERROR!! >>> ", err)
+	} else {
+		fmt.Println(s)
+	}
+}
+
+func main() {
+
+	display(createJob(4, 2))
+	display(createJob(-4, -2))
+	display(createJob(-4, 2))
+	display(createJob(4, -2))
+
+}
---
+++ Concurrent/Future/test.go
@@ -0,0 +1,120 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+type successFunction func(string)
+type errorFunction func(error)
+
+type future struct {
+	successFunc successFunction
+	failFunc    errorFunction
+	wg          *sync.WaitGroup
+}
+
+func (f *future) execute(fn func(val int) (string, error), val int) {
+	f.init()
+	f.wg.Add(1)
+	go func(f *future) {
+		if str, err := fn(val); err != nil {
+			f.failFunc(err)
+			f.wg.Done()
+		} else {
+			f.successFunc(str)
+			f.wg.Done()
+		}
+	}(f)
+}
+
+// func (f *future) execute(fn func() (string, error), val int) {
+// 	f.init()
+// 	f.wg.Add(1)
+// 	go func(f *future) {
+// 		if str, err := fn(val); err != nil {
+// 			f.failFunc(err)
+// 			f.wg.Done()
+// 		} else {
+// 			f.successFunc(str)
+// 			f.wg.Done()
+// 		}
+// 	}(f)
+// }
+
+func (f *future) init() {
+	if f.wg == nil {
+		var wg sync.WaitGroup
+		f.wg = &wg
+	}
+	if f.failFunc == nil {
+		f.failFunc = func(err error) {
+			fmt.Println("ERROR !!! >> ", err)
+		}
+	}
+	if f.successFunc == nil {
+		f.successFunc = func(str string) {
+			fmt.Println(str)
+		}
+	}
+}
+
+func (f *future) success(fn successFunction) *future {
+	f.successFunc = fn
+	return f
+}
+
+func (f *future) fail(fn errorFunction) *future {
+	f.failFunc = fn
+	return f
+}
+
+func funcToExecute1(val int) (string, error) {
+	time.Sleep(time.Second * 2)
+	if val > 10 {
+		return "", fmt.Errorf("value too high for func1! : %v", val)
+	}
+	return "value is correct for func1!", nil
+}
+
+func funcToExecute2(val int) (string, error) {
+	time.Sleep(time.Second * 3)
+	if val%2 != 0 {
+		return "", fmt.Errorf("value not divisible by 2! : %v", val)
+	}
+	return "value is correct for func2!", nil
+}
+
+func funcToExecute3() (string, error) {
+	return "func3 always passes!", nil
+}
+
+func main() {
+
+	futures := &future{}
+
+	val := 40
+
+	futures.success(func(str string) {
+		fmt.Println(str)
+	}).fail(func(err error) {
+		fmt.Println("ERROR !!! >> ", err)
+	}).execute(funcToExecute1, val)
+
+	futures.success(func(str string) {
+		fmt.Println(str)
+	}).fail(func(err error) {
+		fmt.Println("ERROR !!! >> ", err)
+	}).execute(funcToExecute2, val)
+
+	futures.success(func(str string) {
+		fmt.Println(str)
+	}).fail(func(err error) {
+		fmt.Println("ERROR !!! >> ", err)
+	}).execute(func(int) (string, error) {
+		return funcToExecute3()
+	}, 0)
+
+	futures.wg.Wait()
+}
