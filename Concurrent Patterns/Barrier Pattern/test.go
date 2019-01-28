983f69ddc861195ff27fc61856d0cc84b38fe5dd
Prashant Gupta <prashantgupta@us.ibm.com>
2019-01-27 17:03:07 -0800

    custom error in barrier pattern

--- Concurrent Patterns/Barrier Pattern/test.go
+++ Concurrent Patterns/Barrier Pattern/test.go
@@ -6,7 +6,23 @@ import (
 	"time"
 )
 
-type returnVal struct {
+type customError struct {
+	err      string
+	critical bool
+}
+
+func (e *customError) Error() string {
+	return e.err
+}
+
+func customErrorNew(text string, critical bool) error {
+	return &customError{
+		err:      text,
+		critical: critical,
+	}
+}
+
+type result struct {
 	msg string
 	err error
 }
@@ -16,14 +32,14 @@ type functionType func(int) (string, error)
 
 type barrier struct {
 	wg        *sync.WaitGroup
-	results   chan *returnVal
+	results   chan *result
 	functions []functionType
 }
 
 func (b *barrier) init() {
 	var wg sync.WaitGroup
 	b.wg = &wg
-	b.results = make(chan *returnVal)
+	b.results = make(chan *result)
 }
 
 func (b *barrier) add(fn functionType) *barrier {
@@ -31,24 +47,25 @@ func (b *barrier) add(fn functionType) *barrier {
 	return b
 }
 
-func (b *barrier) executeAndReturn(val int) []*returnVal {
+func (b *barrier) executeAndReturn(val int) []*result {
 	b.executeDefault(&val)
 	return b.wait()
 }
 
 func (b *barrier) executeDefault(val *int) {
+	b.init()
 	for _, fn := range b.functions {
 		b.wg.Add(1)
 		go func(fn functionType, b *barrier, val *int) {
 			defer b.wg.Done()
 			resp, err := fn(*val)
 			if err != nil {
-				b.results <- &returnVal{
+				b.results <- &result{
 					msg: "",
 					err: err,
 				}
 			} else {
-				b.results <- &returnVal{
+				b.results <- &result{
 					msg: resp,
 					err: nil,
 				}
@@ -59,27 +76,27 @@ func (b *barrier) executeDefault(val *int) {
 
 func (b *barrier) execute(val int) (string, error) {
 	b.executeDefault(&val)
-	returnValues := b.wait()
+	results := b.wait()
 
-	for _, returnValue := range returnValues {
-		if returnValue.err != nil {
-			return "", returnValue.err
+	for _, result := range results {
+		if result.err != nil {
+			return "", result.err
 		}
 	}
 	return fmt.Sprintf("Values are correct!"), nil
 }
 
-func (b *barrier) wait() []*returnVal {
-	go func(wg *sync.WaitGroup, results chan *returnVal) {
+func (b *barrier) wait() []*result {
+	go func(wg *sync.WaitGroup, results chan *result) {
 		wg.Wait()
 		close(results)
 	}(b.wg, b.results)
 
-	var returnValues []*returnVal
+	var results []*result
 	for result := range b.results {
-		returnValues = append(returnValues, result)
+		results = append(results, result)
 	}
-	return returnValues
+	return results
 }
 
 func job1(val int) (string, error) {
@@ -88,8 +105,9 @@ func job1(val int) (string, error) {
 	if val > 10 {
 		return "success", nil
 	}
-	err := fmt.Errorf("too less for val in func1 : %v. It needs greater than 10 ", val)
-	return "", err
+
+	errMsg := fmt.Sprintf("too less for val in func1 : %v. It needs greater than 10 ", val)
+	return "", customErrorNew(errMsg, false)
 }
 
 func job2(val int) (string, error) {
@@ -98,45 +116,71 @@ func job2(val int) (string, error) {
 	if val%2 == 0 {
 		return "success", nil
 	}
-	err := fmt.Errorf("Val not divisible by 2 in func2 : %v", val)
-	return "", err
+	errMsg := fmt.Sprintf("Val not divisible by 2 in func2 : %v", val)
+	return "", customErrorNew(errMsg, true)
 }
 
-func job3(val int) (string, error) {
+func job3() (string, error) {
 	fmt.Println("executing job3")
 	time.Sleep(time.Second * 2)
 	return "func3 always passes!", nil
 }
 
-func createJobs(val int) (string, error) {
+func handleJobs(val int) {
 
 	barrier := &barrier{}
-	barrier.init()
 
-	barrier.add(job1).add(job2).add(job3)
-
-	//option1, only know if error occured in any of the jobs
-	//return barrier.execute(val)
-
-	//option2, more control on return values
-	returnValues := barrier.executeAndReturn(val)
-	for _, returnValue := range returnValues {
-		if returnValue.err != nil {
-			return "", returnValue.err
-		}
+	job3Wrapper := func(int) (string, error) {
+		return job3()
 	}
-	return fmt.Sprintf("Values are correct!"), nil
-}
 
-func display(s string, err error) {
+	barrier.add(job1).add(job2).add(job3Wrapper)
+
+	//option1, we only care if any critical errors occured in any of the jobs
+	resp, err := barrier.execute(val)
 	if err != nil {
-		fmt.Println("ERROR!! >>> ", err)
+		if err, ok := err.(*customError); ok {
+			if err.critical {
+				fmt.Println("CRITICAL ERROR!! ", err)
+			} else {
+				fmt.Println("ERROR!! ", err)
+			}
+		} else {
+			fmt.Println("ERROR!! >>> ", err)
+		}
 	} else {
-		fmt.Println(s)
+		//SUCCESS, all jobs passed without errors
+		fmt.Println(resp)
 	}
+
+	//option2, if we need more control, we get the list of results
+	// and execute based on each result
+	// results := barrier.executeAndReturn(val)
+
+	// hasError := false
+	// for _, result := range results {
+	// 	if result.err != nil {
+	// 		hasError = true
+	// 		if err, ok := result.err.(*customError); ok {
+	// 			if err.critical {
+	// 				fmt.Println("CRITICAL ERROR!! ", err)
+	// 			} else {
+	// 				fmt.Println("ERROR!! ", err)
+	// 			}
+	// 		} else {
+	// 			fmt.Println("ERROR!! >>> ", result.err)
+	// 		}
+	// 	}
+	// }
+	// if !hasError {
+	// 	fmt.Println("Values are correct!")
+	// }
 }
 
 func main() {
-	display(createJobs(4))
-	display(createJobs(12))
+	handleJobs(4)
+	handleJobs(11)
+	handleJobs(12)
 }
+
+//TODO custom error, interface input
