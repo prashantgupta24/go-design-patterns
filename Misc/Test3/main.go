044bda63d9c5eaf5582219c9dc40b69282656704
Prashant Gupta <prashantgupta@us.ibm.com>
2019-01-31 16:30:51 -0800

    pretty printing struct

--- Misc/Test3/main.go
+++ Misc/Test3/main.go
@@ -1,17 +1,49 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 type tt struct {
 	s string
+	a int
 }
 
-func (t *tt) init() {
-	fmt.Println("init")
-	t.s = "abc"
+// func init() {
+// 	fmt.Println("init")
+// }
+
+func prettyPrintStructString(structString string) {
+	braceReplacer := strings.NewReplacer("{", "\n\n",
+		"}", "\n")
+	replacedString := braceReplacer.Replace(structString)
+	splitStrings := strings.Split(replacedString, ",")
+
+	for _, str := range splitStrings {
+		//trimmedString := strings.Trim(str, " ")
+		fmt.Printf(str + "\n")
+	}
 }
+
 func main() {
 	t := &tt{}
-	t.init()
-	fmt.Println("main")
+	t.s = "test"
+	t.a = 123
+
+	s := fmt.Sprintf("%#v", t)
+	prettyPrintStructString(s)
+	rep := strings.NewReplacer("{", "\n\n",
+		"}", "\n")
+	s1 := rep.Replace(s)
+	sp := strings.Split(s1, ",")
+	for _, str := range sp {
+		st := strings.Trim(str, " ")
+		fmt.Printf(st + "\n")
+	}
+
+	// s1 := (strings.Replace(s, "{", "\n", -1)).Replace(s1, "}", "\n", -1)
+	// s1 = strings.Replace(s1, "}", "\n", -1)
+
+	//fmt.Println(s)
 }
