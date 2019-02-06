3d14071824cc6d0ecf6c1a2ffb03a5b7b5d80618
Prashant Gupta <prashantgupta@us.ibm.com>
2019-02-05 20:26:03 -0800

    refactored future pattern

--- Concurrent Patterns/Future Pattern/future_test.go
+++ Concurrent Patterns/Future Pattern/future_test.go
@@ -2,9 +2,95 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
 )
 
-func Test(t *testing.T) {
-	fmt.Println("testing")
+const (
+	sleepTime        = 5
+	numLoops         = 100
+	randomValueLimit = 100
+)
+
+type testCase struct {
+	suite.Suite
+}
+
+func TestSuite(t *testing.T) {
+	suite.Run(t, new(testCase))
+}
+
+func (suite *testCase) TestFuture1() {
+	t := suite.T()
+
+	var successes int
+	var failures int
+
+	for i := 0; i < numLoops; i++ {
+		val := rand.Intn(randomValueLimit)
+
+		NewFuture().success(func(str string) {
+			fmt.Println("func1 was successful")
+			successes++
+		}).fail(func(err error) {
+			fmt.Printf("error returned from func1 %v\n", err)
+			failures++
+		}).execute(funcToExecute1, val)
+
+		NewFuture().success(func(str string) {
+			fmt.Println("func2 was successful")
+			successes++
+		}).fail(func(err error) {
+			fmt.Printf("error returned from func2 %v\n", err)
+			failures++
+		}).execute(funcToExecute2, val)
+
+		func3Wrap := func(int) (string, error) {
+			return funcToExecute3()
+		}
+		NewFuture().success(func(str string) {
+			fmt.Println("func3 was successful")
+			successes++
+		}).fail(func(err error) {
+			fmt.Printf("error returned from func3 %v\n", err)
+			failures++
+		}).execute(func3Wrap, val)
+	}
+
+	time.Sleep(time.Second * time.Duration(sleepTime))
+
+	totalOutcomes := successes + failures
+	assert.Equal(t, numLoops*3, totalOutcomes, "success and failures should be %v, instead it is %v", numLoops*3, totalOutcomes)
+}
+
+func sleep() {
+	time.Sleep(time.Second * time.Duration(rand.Intn(sleepTime)))
+}
+
+func funcToExecute1(val int) (string, error) {
+	//fmt.Println("func1")
+	sleep()
+	if val > 10 {
+		return "", fmt.Errorf("value too high for func1! : %v", val)
+	}
+	return "value is correct for func1!", nil
+}
+
+func funcToExecute2(val int) (string, error) {
+	//fmt.Println("func2")
+	sleep()
+	if val%2 != 0 {
+		return "", fmt.Errorf("value not divisible by 2! : %v", val)
+	}
+	return "value is correct for func2!", nil
+}
+
+func funcToExecute3() (string, error) {
+	//fmt.Println("func3")
+	sleep()
+	return "func3 always passes!", nil
 }
--- Concurrent Patterns/Future Pattern/main.go
+++ Concurrent Patterns/Future Pattern/main.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"fmt"
-	"sync"
-	"time"
 )
 
 //all functions need to be of this type
@@ -15,28 +13,26 @@ type errorFunction func(error)
 type future struct {
 	successFunc successFunction
 	failFunc    errorFunction
-	wg          *sync.WaitGroup
 }
 
-func (f *future) execute(fn functionType, val int) {
+//NewFuture creates a new future
+func NewFuture() *future {
+	return &future{}
+}
+
+func (f *future) execute(fn functionType, val int) *future {
 	f.init()
-	f.wg.Add(1)
 	go func(f *future) {
 		if str, err := fn(val); err != nil {
 			f.failFunc(err)
-			f.wg.Done()
 		} else {
 			f.successFunc(str)
-			f.wg.Done()
 		}
 	}(f)
+	return f
 }
 
 func (f *future) init() {
-	if f.wg == nil {
-		var wg sync.WaitGroup
-		f.wg = &wg
-	}
 	if f.failFunc == nil {
 		f.failFunc = func(err error) {
 			fmt.Println("ERROR !!! >> ", err)
@@ -59,51 +55,6 @@ func (f *future) fail(fn errorFunction) *future {
 	return f
 }
 
-func funcToExecute1(val int) (string, error) {
-	time.Sleep(time.Second * 2)
-	if val > 10 {
-		return "", fmt.Errorf("value too high for func1! : %v", val)
-	}
-	return "value is correct for func1!", nil
-}
-
-func funcToExecute2(val int) (string, error) {
-	time.Sleep(time.Second * 3)
-	if val%2 != 0 {
-		return "", fmt.Errorf("value not divisible by 2! : %v", val)
-	}
-	return "value is correct for func2!", nil
-}
-
-func funcToExecute3() (string, error) {
-	return "func3 always passes!", nil
-}
-
 func main() {
 
-	futures := &future{}
-
-	val := 40
-
-	futures.success(func(str string) {
-		fmt.Println(str)
-	}).fail(func(err error) {
-		fmt.Println("ERROR !!! >> ", err)
-	}).execute(funcToExecute1, val)
-
-	futures.success(func(str string) {
-		fmt.Println(str)
-	}).fail(func(err error) {
-		fmt.Println("ERROR !!! >> ", err)
-	}).execute(funcToExecute2, val)
-
-	futures.success(func(str string) {
-		fmt.Println(str)
-	}).fail(func(err error) {
-		fmt.Println("ERROR !!! >> ", err)
-	}).execute(func(int) (string, error) {
-		return funcToExecute3()
-	}, 0)
-
-	futures.wg.Wait()
 }
